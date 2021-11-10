28cad4faf80e2c6614b083467b297087e7eb9037
Whyvilos <vlad03.01@mail.ru>
2021-11-10 21:57:49 +0300

    created default compose app

---
+++ app/src/main/java/com/example/iot_application/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.iot_application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.iot_application.ui.theme.Iot_applicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Iot_applicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Iot_applicationTheme {
+        Greeting("Android")
+    }
+}
